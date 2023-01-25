1045308e899516c8186f9340dca499c5d1fa11f2
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-01-25 09:53:27 -0300

    exercicio js

---
+++ scripts.js
@@ -0,0 +1,30 @@
+//function boasVindas(){
+// document.write("Seja bem vindo Felipe");
+//}
+//boasVindas()
+
+//function calcularidade(ano, mes){
+  //  let anoAtual = 2023;
+  //  let mesAtual = "janeiro";
+  //  if(mes == mesAtual){
+   // document.write("thiago tem" + (anoAtual - ano)+"anos")
+  //  }else{
+//document.write("thiago tem" + (anoAtual - ano -1)+"anos")
+//  } 
+//} 
+
+//calcularidade(1993,"dezembro"); 
+
+//function percentualSala(alunos, faltaram){
+  //let percentual = ((faltaram / alunos) * 100.tofixed(0)) ;
+ // return percentual;
+//
+//document.write(percentualSala(19,8).toFixed(0) + "% dos alunos")
+
+//function media(teorica = 0, pratica1 = 0, pratica2 = 0){
+   // let pratica = (pratica1 + pratica2) / 2;
+   // let nota = (teorica + pratica) / 2;
+   // return nota;
+//}
+//document.write("a media do thiago e:"+ media(2,8,6));
+
